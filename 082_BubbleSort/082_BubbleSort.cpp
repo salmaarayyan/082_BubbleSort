96e9d3e53eb6be974dfa39a95b52fa50cad86e2c
salmaarayyan <salmaarayyan@gmail.com>
2024-03-08 15:11:49 +0700

    deklarasi variabel global

--- 082_BubbleSort/082_BubbleSort.cpp
+++ 082_BubbleSort/082_BubbleSort.cpp
@@ -2,6 +2,11 @@
 #include <string>
 using namespace std;
 
+
+
+int arr[20]; //deklarasi variabel global array a dengan ukuran 20
+int n;  //deklarasi variabel global n untuk menyimpan banyaknya elemen array
+
 int main()
 {
     std::cout << "Hello World!\n";
