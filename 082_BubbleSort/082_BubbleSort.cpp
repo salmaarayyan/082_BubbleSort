3a39bd6120590d4cdfd3b2b482c247a2a17c52a9
salmaarayyan <salmaarayyan@gmail.com>
2024-03-08 15:44:13 +0700

    membuat prosedur bubblesort

--- 082_BubbleSort/082_BubbleSort.cpp
+++ 082_BubbleSort/082_BubbleSort.cpp
@@ -31,7 +31,28 @@ void input() {  //prosedur untuk input
     }
 }
 
+void bubbleSortArray() {  //prosedur untuk mengurutkan array dengan metode bubble sort
+    int pass = 1;  //step 1
 
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {  //step 2
+            if (arr[j] > arr[j + 1]) {  //step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+            }
+        }
+        pass = pass + 1;  //step 4
+
+        cout << "\nPass" << pass - 1 << ": ";  //number of pass
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";  //menampilkan data pada number of pass
+        }
+        cout << endl;
+    } while (pass <= n - 1);  //step 5
+}
 
 
 
